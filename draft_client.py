90e4edc500990545b51128082756f7fabcecd54a
husisy <zchaozhang@outlook.com>
2023-09-08 11:53:12 +0800

    add apikey auth

--- draft_client.py
+++ draft_client.py
@@ -8,14 +8,13 @@ dotenv.load_dotenv()
 FASTAPI_KEY = [x for x in os.environ['SIGNED_API_KEY'].split(',') if x][0]
 SERVER_IP = os.environ['SERVER_IP']
 SERVER_PORT = 9803
+USE_HTTPS = False
 
 def hf_fastapi_chatgpt(sentence, temperature=0.3):
-    headers = {'Authorization': f'Bearer {FASTAPI_KEY}', 'Content-Type': 'application/json', 'accept':'application/json'}
+    headers = {'Authorization': f'{FASTAPI_KEY}', 'Content-Type': 'application/json', 'accept':'application/json'}
     data = {'sentence': sentence, 'temperature':temperature}
-    if isinstance(sentence, str):
-        url = f'http://{SERVER_IP}:{SERVER_PORT}/gpt/oneround'
-    else:
-        url = f'http://{SERVER_IP}:{SERVER_PORT}/gpt/multiround'
+    tmp0 = 'oneround' if isinstance(sentence, str) else 'multiround'
+    url = ('https' if USE_HTTPS else 'http') + f'://{SERVER_IP}:{SERVER_PORT}/gpt/{tmp0}'
     ret = requests.get(url, data=json.dumps(data), headers=headers).json()
     return ret
 
--- draft_server.py
+++ draft_server.py
@@ -1,5 +1,4 @@
 import os
-import time
 import typing
 import dotenv
 import fastapi
@@ -12,13 +11,9 @@ SIGNED_API_KEY = {str(x) for x in os.environ.get('SIGNED_API_KEY','').split(',')
 # python -c 'import secrets; print(secrets.token_hex(32))'
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-oauth2_scheme = fastapi.security.OAuth2PasswordBearer(tokenUrl="token")
-
-
-def api_key_auth(api_key: str = fastapi.Depends(oauth2_scheme)):
-    if api_key not in SIGNED_API_KEY:
-        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_401_UNAUTHORIZED, detail="Forbidden")
-
+def verify_apikey(Authorization: typing.Annotated[str, fastapi.Header()]):
+    if str(Authorization) not in SIGNED_API_KEY:
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
 
 class ChatGPTSentence(pydantic.BaseModel):
     role: str
@@ -40,7 +35,7 @@ class ChatGPTConversationReply(pydantic.BaseModel):
 app = fastapi.FastAPI()
 
 
-@app.get("/gpt/multiround", dependencies=[fastapi.Depends(api_key_auth)])
+@app.get("/gpt/multiround", dependencies=[fastapi.Depends(verify_apikey)])
 def get_gpt_multiround(chat: ChatGPTConversation)->ChatGPTConversationReply:
     tmp0 = chat.model_dump()['sentence']
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=tmp0, temperature=chat.temperature)
@@ -49,7 +44,7 @@ def get_gpt_multiround(chat: ChatGPTConversation)->ChatGPTConversationReply:
             'completion_tokens': response['usage']['completion_tokens']}
     return ret
 
-@app.get("/gpt/oneround")
+@app.get("/gpt/oneround", dependencies=[fastapi.Depends(verify_apikey)])
 def get_gpt_oneround(message: ChatGPTOneSentenceOnly)->ChatGPTConversationReply:
     if message.sentence:
         tmp0 = [
@@ -64,4 +59,4 @@ def get_gpt_oneround(message: ChatGPTOneSentenceOnly)->ChatGPTConversationReply:
         ret = {'response': '', 'total_tokens':0, 'completion_tokens': 0}
     return ret
 
-# uvicorn draft_server:app --port 9803 --host 0.0.0.0
+# uvicorn draft_server:app --port 9803
