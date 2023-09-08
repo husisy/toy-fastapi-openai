f48eed9040a3417cefa4fc9ed38c529375168442
husisy <zchaozhang@outlook.com>
2023-09-08 12:07:25 +0800

    support https

--- draft_client.py
+++ draft_client.py
@@ -7,14 +7,16 @@ dotenv.load_dotenv()
 
 FASTAPI_KEY = [x for x in os.environ['SIGNED_API_KEY'].split(',') if x][0]
 SERVER_IP = os.environ['SERVER_IP']
-SERVER_PORT = 9803
+SERVER_PORT = None
 USE_HTTPS = False
+
+tmp0 = '' if (SERVER_PORT is None) else f':{SERVER_PORT}'
+API_URL = ('https' if USE_HTTPS else 'http') + f'://{SERVER_IP}{tmp0}/gpt/'
 
 def hf_fastapi_chatgpt(sentence, temperature=0.3):
     headers = {'Authorization': f'{FASTAPI_KEY}', 'Content-Type': 'application/json', 'accept':'application/json'}
     data = {'sentence': sentence, 'temperature':temperature}
-    tmp0 = 'oneround' if isinstance(sentence, str) else 'multiround'
-    url = ('https' if USE_HTTPS else 'http') + f'://{SERVER_IP}:{SERVER_PORT}/gpt/{tmp0}'
+    url = API_URL + ('oneround' if isinstance(sentence, str) else 'multiround')
     ret = requests.get(url, data=json.dumps(data), headers=headers).json()
     return ret
 
